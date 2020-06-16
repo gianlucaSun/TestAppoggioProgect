b8a3b2bef0b151fc275fa005d7a341f2af848179
Gianluca <gianlucager@libero.it>
2020-06-17 00:34:28 +0200

    mod pippo pippo

--- src/it/management/Management.java
+++ src/it/management/Management.java
@@ -2,8 +2,12 @@ package it.management;
 
 public class Management {
 	
-	private String name;
+  private String name;
   
   private String cognome;
+  
+  private boolean connettore;
+	
+  private int age;	
 
 }
