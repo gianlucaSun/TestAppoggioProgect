78072ca1b1adad08430a563ce0b694baf69799aa
Gianluca <gianlucager@libero.it>
2020-06-17 00:55:21 +0200

    mod.

--- src/it/management/Management.java
+++ src/it/management/Management.java
@@ -1,14 +1,13 @@
 package it.management;
 
 public class Management {
-	
-private String name;
+
+ private String name;
   
   private String cognome;
   
   private boolean connettore;
 	 
   private int age;	
-
 
 }
